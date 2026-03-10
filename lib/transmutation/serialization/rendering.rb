89a06a375e30eb80f71f709375d975d58e3105e8
Mordechai <mordwein77@gmail.com>
2026-03-10 16:23:14 -0400

    Fix send_data render keyword forwarding

--- lib/transmutation/serialization/rendering.rb
+++ lib/transmutation/serialization/rendering.rb
@@ -5,7 +5,7 @@ module Transmutation
     module Rendering
       # Serializes the value of the `json` parameter before calling the existing render method.
       def render(json: nil, serialize: true, namespace: nil, serializer: nil, max_depth: 1, **args)
-        return super(**args) unless json
+        return super(json:, **args) unless json
         return super(**args, json:) unless serialize
 
         super(**args, json: serialize(json, namespace:, serializer:, max_depth:))
