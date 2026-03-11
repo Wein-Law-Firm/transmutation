6ffccfd90d72689fc4a5c288d1b0e6e2bbcb11ab
Mordechai <mordwein77@gmail.com>
2026-03-10 23:53:02 -0400

    Revert Serializer initializer to upstream default max_depth: 1

--- lib/transmutation/serializer.rb
+++ lib/transmutation/serializer.rb
@@ -20,7 +20,7 @@ module Transmutation
 
     include Transmutation::Serialization
 
-    def initialize(object, depth: 0, max_depth: Transmutation.max_depth)
+    def initialize(object, depth: 0, max_depth: 1)
       @object = object
       @depth = depth
       @max_depth = max_depth
